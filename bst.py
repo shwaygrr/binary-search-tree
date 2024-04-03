4047560e04b6491dba32f3b70c81c75c219a9667
Chwoeger Charleston <122495108+shwaygrr@users.noreply.github.com>
2024-04-03 18:38:40 -0400

    clases and constructors

---
+++ bst.py
@@ -0,0 +1,25 @@
+#Class Implementation
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self, root_val):
+        self.root = Node(root_val)
+
+
+
+#create tree
+
+
+
+#insert node
+
+
+
+#delete node
+
+
+#print tree
---
+++ main.py
@@ -0,0 +1,5 @@
+from bst import *
+
+# node = Node(10, 10)
+tree = BinarySearchTree(10)
+
